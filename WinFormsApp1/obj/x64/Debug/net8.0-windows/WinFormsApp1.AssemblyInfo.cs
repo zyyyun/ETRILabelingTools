55a08eeb7bcf9517c3ed8e06282b49e563a7a230
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-22 18:17:17 +0900

    feat: yolo 추적 기능 개선

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c024869fba41da7869f319b8228a8f2d1f78209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd84cc54f51c17306d6e848f6bd27e43271e51e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
