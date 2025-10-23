c003cd3232eec6cddc541b2e79ce5d5a3a7b4e4b
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-23 15:07:50 +0900

    perf: 캐시 사용으로 렌더링 성능 개선

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1b6caa8abe55f8a825139970780f1b01808ccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d0188af21031017d813e9ef1923a0bdca435fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
