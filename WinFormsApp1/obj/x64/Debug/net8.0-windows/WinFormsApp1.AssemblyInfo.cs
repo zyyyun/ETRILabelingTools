60fe14f88dbe7492234ebfc168a4e4d3b63bce0d
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-23 17:19:15 +0900

    fix: eventbox관련 버그 개선

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01f9f49682628e48ec893fb77efdc1c3dbd9e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248ac60861f2c8a1a86e04b412ebbeae337199af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
