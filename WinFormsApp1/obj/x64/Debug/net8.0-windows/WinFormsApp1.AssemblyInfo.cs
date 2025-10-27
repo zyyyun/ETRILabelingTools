206f3600187cc4b719349a1ee0c6b776e45ef806
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-27 13:46:38 +0900

    feat: 창 이동 기능

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9fe55331a0b48d80daa4998dcfabef22c0a50e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d30b847f1111d9e1c822786ab8423415576e86f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
