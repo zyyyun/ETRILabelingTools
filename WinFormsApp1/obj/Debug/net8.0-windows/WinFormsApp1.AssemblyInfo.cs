a2dd1bf27c5feb2fa7a799d6fa33a9d2eb3f5f2e
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-28 17:52:25 +0900

    fix: vehicle box가 한 프레임만 삭제되는 오류 수정

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e680dfaf5b36912e7f9fd0d9f4d3f4b0ac5f18c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67500fcf24878c24a5b02ec240ca4b3341f095d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
