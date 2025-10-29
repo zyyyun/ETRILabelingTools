962ee3cb2c108175220e5cb5ad94617f770fbb8b
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-29 18:13:59 +0900

    feat: 관성추적2 (미완)

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d820ca85819656a6f671c37614aab3ea675c3a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfcf52112501134f2cbe5bb4b6d3267363d53f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
