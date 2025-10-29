aa31d037fa18f09eb3bb68590bf4be94e2d875f0
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-29 17:09:31 +0900

    fix: json 저장 관련 버그 수정

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a89cd312fcdaf5cb3f6f0b2bcbd0f80380976ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb3b15e31cbfaf3cb645dddd2ec531fe81d595d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
