5f86734f0d31be5a7dadaaa204d1fbf27025448b
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-30 13:48:59 +0900

    fix: 박스 선택 버그 해결

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962ee3cb2c108175220e5cb5ad94617f770fbb8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636726dcd59a3da2abf0315b2132c9efe2304987")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
