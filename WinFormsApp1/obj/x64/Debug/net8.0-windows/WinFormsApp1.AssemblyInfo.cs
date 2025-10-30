4f5194bb6914953537ea7c8b8546420455369d13
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-30 14:46:26 +0900

    feat: event 로직 수정

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636726dcd59a3da2abf0315b2132c9efe2304987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2132cb57eed91ace0b05f0062a23ecc35e1b5781")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
