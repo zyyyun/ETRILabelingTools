a429311d3ecb4630c42c7d548976dc3d9b84a659
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-10-31 10:49:26 +0900

    fix: waypoint 패널 수정

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e1468bad3179f555927a890d40ca7549cea6b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb60547fd78b4592cca74f481b53b9f266f37d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
