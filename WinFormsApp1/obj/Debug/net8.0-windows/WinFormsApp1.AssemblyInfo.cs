e06102299bd03ef66ae0b772e321524ea663a354
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-11-03 14:04:04 +0900

    feat: waypoint 유연성

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44a1f1530b5b8e85eb0fd3c36c33649405757c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6faae4ef6eebb8e82b4620488ee4b7b30a9ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
