dd8379e97ff8405ea18b7cc0329adc9bc1c31d2e
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-11-03 16:47:41 +0900

    design: 사이드바 디자인 개선

--- WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/x64/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06102299bd03ef66ae0b772e321524ea663a354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271138c127d3cc3f0fec49e03dbde11e94c7fffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
