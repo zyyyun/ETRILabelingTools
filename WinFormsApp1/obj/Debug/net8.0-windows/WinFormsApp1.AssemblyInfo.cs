c57cb8c580e8071198166c2f539fc84022606753
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-11-05 10:31:55 +0900

    feat: waypoint id 수정 기능

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2451133fced9f0e2e9c8182f31c73256ebb63d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6151aba1ac006ccd881388fdc5ab0cc243c67d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
