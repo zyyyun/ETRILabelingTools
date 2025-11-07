88256440b848c1e8891ff03a61185d79ff3136e6
DESKTOP-SLNOEDA\ANNA <younseu2@gmail.com>
2025-11-07 18:57:47 +0900

    fix: 오류 수정

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3310298f1f242aa2abca894f1fb2f50f01be6e90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ef194af413688e6354c071c196fba1ab57cd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
